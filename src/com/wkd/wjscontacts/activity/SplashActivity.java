0389884f0948718113fe5a3d7e63fe3f1133e95e
shang1jk <shang1jk@gmail.com>
2014-08-14 18:33:10 +0800

    版本更新功能

--- src/com/wkd/wjscontacts/activity/SplashActivity.java
+++ src/com/wkd/wjscontacts/activity/SplashActivity.java
@@ -1,22 +1,73 @@
 package com.wkd.wjscontacts.activity;
 
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 
-import android.app.DownloadManager;
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnClickListener;
+import android.content.Intent;
 import android.net.Uri;
+import android.os.Environment;
+import android.os.Handler;
+import android.os.Message;
+import android.widget.RemoteViews;
 
 import com.avos.avoscloud.AVAnalytics;
 import com.avos.avoscloud.AVException;
 import com.avos.avoscloud.AVObject;
 import com.avos.avoscloud.AVQuery;
 import com.avos.avoscloud.GetCallback;
+import com.avos.avoscloud.LogUtil.log;
 import com.wkd.wjscontacts.R;
 import com.wkd.wjscontacts.utils.CommonUtil;
 import com.wkd.wjscontacts.utils.PromptUtil;
 
 public class SplashActivity extends BaseActivity {
+	protected static final int MSG_UPDATE_PROGRESS = 1;		//更新下载进度
+	private final int NOTIFICATION_ID_DOWNLOAD = 0;
+	
+	private NotificationManager nManager;
+	private Notification noti;
+	private File apkFile;
+	
+	private Handler handler = new Handler(){
 
+		@Override
+		public void handleMessage(Message msg) {
+			super.handleMessage(msg);
+			
+			switch (msg.what) {
+			case MSG_UPDATE_PROGRESS:
+				
+				RemoteViews remoteViews = new RemoteViews(getPackageName(), R.layout.download_noti_layout);
+				remoteViews.setProgressBar(R.id.pb_download_1, 100, (Integer)msg.obj, false);
+				remoteViews.setProgressBar(R.id.pb_download_2, 100, (Integer)msg.obj, false);
+				remoteViews.setProgressBar(R.id.pb_download_3, 100, (Integer)msg.obj, false);
+				remoteViews.setProgressBar(R.id.pb_download_4, 100, (Integer)msg.obj, false);
+				remoteViews.setProgressBar(R.id.pb_download_5, 100, (Integer)msg.obj, false);
+				log.e("xxx", msg.obj.toString());
+				noti.contentView = remoteViews;
+				nManager.notify(NOTIFICATION_ID_DOWNLOAD, noti);
+				
+				break;
+
+			default:
+				break;
+			}
+		}
+	};
+	
 	@Override
 	protected void setContentLayout() {
 		setContentView(R.layout.splashactivity_layout);
@@ -26,6 +77,12 @@ public class SplashActivity extends BaseActivity {
 	protected void beforeInitView() {
 		//统计
 		AVAnalytics.trackAppOpened(getIntent());
+		
+		nManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+		noti = new Notification();
+		noti.icon = R.drawable.ic_launcher;
+		noti.tickerText = "微金所通讯录";
+		noti.flags = Notification.FLAG_AUTO_CANCEL;
 	}
 
 	@Override
@@ -40,13 +97,24 @@ public class SplashActivity extends BaseActivity {
 		avQuery.orderByDescending("appVer").getFirstInBackground(new GetCallback<AVObject>() {
 			
 			@Override
-			public void done(AVObject avObject, AVException e) {
+			public void done(final AVObject avObject, AVException e) {
 				if (e == null) {
 					int currVer	= CommonUtil.getAppVer(SplashActivity.this);
 					int lastestVer	= avObject.getInt("appVer");
 					if (currVer < lastestVer) {	//有新版本
 						//下载并安装新版本
-						doDownloadAndInstall(avObject.getString("url"));
+						PromptUtil.showDialog(SplashActivity.this,
+								"发现新版本", avObject.getString("desc"),
+								"更新", "就不", new OnClickListener() {
+
+									@Override
+									public void onClick(
+											DialogInterface dialog,
+											int which) {
+										doDownload(avObject.getString("url"));
+										
+									}
+								}, null);
 					}else{
 						//获取最新的联系人数据
 						getNewContacts();
@@ -58,29 +126,79 @@ public class SplashActivity extends BaseActivity {
 				}
 			}
 		});
-		
-		
-		
-	
-		
 
 	}
-	
 
 	
 	/**
 	 * 下载并安装apk
-	 * 调用系统下载管理器下载
 	 */
-	protected void doDownloadAndInstall(String apkUrl) {
-		DownloadManager dManager = (DownloadManager) getSystemService(DOWNLOAD_SERVICE);
-		DownloadManager.Request request = new DownloadManager.Request(Uri.parse(apkUrl));
-		request.setTitle("WJSContacts");
-		request.setDescription("WJSContacts 下载中...");
-        request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
+	protected void doDownload(final String apkUrl) {
 		
+		if (!Environment.MEDIA_MOUNTED.equals(Environment.getExternalStorageState())) {
+			PromptUtil.toast(SplashActivity.this, "没有找到sd卡");
+			return;
+		}
 		
-		long enqueueId = dManager.enqueue(request);
+		new Thread(new Runnable() {
+			
+			@Override
+			public void run() {
+				DefaultHttpClient client = new DefaultHttpClient();
+				HttpUriRequest post = new HttpPost(apkUrl);
+				
+				InputStream is = null;
+				FileOutputStream fos = null;
+				try {
+					HttpResponse resp = client.execute(post);
+					if (HttpStatus.SC_OK == resp.getStatusLine().getStatusCode()) {
+						long apkSize = resp.getEntity().getContentLength();			//文件大小
+						is = resp.getEntity().getContent();
+						String apkDirPath = Environment.getExternalStorageDirectory() + "/WJSContacts";
+						File apkDir = new File(apkDirPath);
+						if (!apkDir.exists()) {
+							apkDir.mkdirs();
+						}
+						apkFile = new File(apkDir+"/WJSContacts.apk");
+						fos = new FileOutputStream(apkFile);
+						
+						byte[] buffer = new byte[256 * 1024];
+						int length = 0, rate = 0;
+						long sum = 0;
+						while((length = is.read(buffer)) != -1){
+							fos.write(buffer, 0, length);
+							sum += length;
+							int rateNow = (int) (sum * 100 / apkSize);
+							if (rateNow > rate) {
+								rate = rateNow;
+								Message msg = Message.obtain();
+								msg.what = MSG_UPDATE_PROGRESS;
+								msg.obj = rate;
+								
+								if (100 == rateNow) {
+									Intent installIntent = getInstallIntent(apkFile);
+									startActivity(installIntent);
+									noti.contentIntent = PendingIntent.getActivity(SplashActivity.this, 0, installIntent, Intent.FLAG_ACTIVITY_NEW_TASK);
+								}
+								
+								handler.sendMessage(msg);
+							}
+						}
+					}
+				} catch (Exception e) {
+					e.printStackTrace();
+				} finally{
+					try {
+						is.close();
+						fos.close();
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+				}
+				
+			}
+		}).start();
+		
 	}
 
 	/**
@@ -97,4 +215,16 @@ public class SplashActivity extends BaseActivity {
 		// TODO Auto-generated method stub
 		
 	}
+	
+	/**
+	 * @return		安装apk的intent
+	 */
+	private Intent getInstallIntent(File file) {
+		Intent installIntent = new Intent();
+		installIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+		installIntent.setAction(android.content.Intent.ACTION_VIEW);
+		installIntent.setDataAndType(Uri.fromFile(file), "application/vnd.android.package-archive");
+		return installIntent;
+	}
+	
 }
--- src/com/wkd/wjscontacts/utils/PromptUtil.java
+++ src/com/wkd/wjscontacts/utils/PromptUtil.java
@@ -1,16 +1,30 @@
 package com.wkd.wjscontacts.utils;
 
+import android.app.AlertDialog;
+import android.app.AlertDialog.Builder;
 import android.content.Context;
+import android.content.DialogInterface.OnClickListener;
 import android.view.Gravity;
 import android.widget.Toast;
 
 public class PromptUtil {
 	
-	/** 居中土司 */
+	/**	  居中土司  */
 	public static void toast(Context ctx, String msg){
 		Toast toast = Toast.makeText(ctx, msg, Toast.LENGTH_SHORT);
 		toast.setGravity(Gravity.CENTER, 0, 0);
 		toast.show();
 	}
-
+	
+	/**  提示对话框  */
+	public static void showDialog(Context ctx, String title, String msg, String ok, String cancel, OnClickListener okListener, OnClickListener cancelListener){
+		
+		Builder builder = new AlertDialog.Builder(ctx);
+		builder.setTitle(title)
+					.setMessage(msg)
+					.setPositiveButton(ok, okListener)
+					.setNegativeButton(cancel, cancelListener)
+					.setCancelable(false)
+					.show();
+	}
 }
